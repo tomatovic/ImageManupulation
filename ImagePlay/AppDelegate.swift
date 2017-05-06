709780c3dc35e2ff5f8afb8a42580d72a458a4d2
 Thomas Hirth  <thomas.hirth@berlin.de>
2017-05-06 19:43:42 +0200

    Change Name

--- ImagePlay/AppDelegate.swift
+++ ImagePlay/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  ImagePlay
 //
-//  Created by Robert Patterson on 2016-03-13.
+//  Created by Thomas Hirth on 2016-03-13.
 //  Copyright © 2016 robbo. All rights reserved.
 //
 
